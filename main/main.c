3b2be0ace3bdeb24e1ec792f97d90d8a39ce80f3
Marcelo Pinheiro <mspinheiro84>
2023-09-14 11:05:44 -0300

    implementação do http request

--- main/main.c
+++ main/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <sys/param.h>
+
 /*includes FreeRTOS*/
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -13,20 +15,139 @@
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "nvs_flash.h"
+#include "esp_tls.h"
 
 #include "protocol_examples_common.h"
 
-/*define dos pinos*/
+#include "esp_http_client.h"
+
+/*define*/
 #define LED 2 /*LED do kit*/
 #define BUTTON 21 /*LED do kit*/
+
+#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048
 
 TaskHandle_t xHandleLed;
 void vTaskContador (void *pvParameters);
 void vTaskLed (void *pvParameters);
+void vTaskHttpRequest (void *pvParameters);
 
 static const char *TAG = "IRRIGACAO";
 
 SemaphoreHandle_t xHandleSemaphore = NULL;
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt){
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            // Clean the buffer in case of a new request
+            if (output_len == 0 && evt->user_data) {
+                // we are just starting to copy the output data into the use
+                memset(evt->user_data, 0, MAX_HTTP_OUTPUT_BUFFER);
+            }
+            /*
+             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+             *  However, event handler can also be used in case chunked encoding is used.
+             */
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                
+                /*=================== imprimir ==================*/
+                printf("%.*s \n\n\n", evt->data_len, (char*)evt->data);
+                /*=====================================*/
+                
+                // If user_data buffer is configured, copy the response into the buffer
+                int copy_len = 0;
+                if (evt->user_data) {
+                    // The last byte in evt->user_data is kept for the NULL character in case of out-of-bound access.
+                    copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
+                    if (copy_len) {
+                        memcpy(evt->user_data + output_len, evt->data, copy_len);
+                    }
+                    
+                } else {
+                    int content_len = esp_http_client_get_content_length(evt->client);
+                    if (output_buffer == NULL) {
+                        // We initialize output_buffer with 0 because it is used by strlen() and similar functions therefore should be null terminated.
+                        output_buffer = (char *) calloc(content_len + 1, sizeof(char));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    copy_len = MIN(evt->data_len, (content_len - output_len));
+                    if (copy_len) {
+                        memcpy(output_buffer + output_len, evt->data, copy_len);
+                    }
+                }
+                output_len += copy_len;
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            if (output_buffer != NULL) {
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_REDIRECT:
+            ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
+            esp_http_client_set_header(evt->client, "From", "user@example.com");
+            esp_http_client_set_header(evt->client, "Accept", "text/html");
+            esp_http_client_set_redirection(evt->client);
+            break;
+    }
+    return ESP_OK;
+}
+
+void http_client_request(){
+    esp_http_client_config_t config = {
+        .url = "http://worldtimeapi.org/api/timezone/America/Recife",
+        .event_handler = _http_event_handler,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK){
+        ESP_LOGI(TAG, "Status = %d, content_length = %d", 
+            esp_http_client_get_status_code(client), 
+            (int)esp_http_client_get_content_length(client));
+    }
+    esp_http_client_cleanup(client);
+}
 
 static void IRAM_ATTR gpio_isr_handle(void *arg){
     BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
@@ -65,16 +186,17 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     example_connect();
-    vTaskDelay(pdMS_TO_TICKS(10000));
     
     xHandleSemaphore = xSemaphoreCreateCounting(5,0);
     if (xHandleSemaphore != pdFALSE){
         configPinos();
         
         TaskHandle_t xHandleContador;
+        TaskHandle_t xHandleHttpRequest;
 
         if ((xTaskCreate(vTaskLed, "LED", configMINIMAL_STACK_SIZE+1024, NULL, 1, &xHandleLed) != pdFAIL) &&
-            (xTaskCreate(vTaskContador, "CONTADOR", configMINIMAL_STACK_SIZE+1024, NULL, 1, &xHandleContador) != pdFAIL)){
+            (xTaskCreate(vTaskContador, "CONTADOR", configMINIMAL_STACK_SIZE+1024, NULL, 1, &xHandleContador) != pdFAIL) &&
+            (xTaskCreate(vTaskHttpRequest, "HTTP-REQUEST", configMINIMAL_STACK_SIZE+1024, NULL, 3, &xHandleHttpRequest) != pdFAIL)){
 
             gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1);
             gpio_isr_handler_add(BUTTON, gpio_isr_handle, NULL);
@@ -82,7 +204,7 @@ void app_main(void)
             while (1)
             {
                 ESP_LOGI(TAG,"====> Aplicação principal <====\n\n");
-                vTaskDelay(pdMS_TO_TICKS(7500));
+                vTaskDelay(pdMS_TO_TICKS(10000));
             }
             
             example_disconnect();
@@ -95,15 +217,25 @@ void app_main(void)
     }
 }
 
+
+void vTaskHttpRequest (void *pvParameters){
+    while (1)
+    {        
+        http_client_request();
+        vTaskDelay(pdMS_TO_TICKS(15000));
+    }
+    
+}
+
 void vTaskContador (void *pvParameters){
     uint8_t cont = 0;
     while (1)
     {
         xSemaphoreTake(xHandleSemaphore, portMAX_DELAY);
-        if (++cont == 10){
+        if (++cont == 5){
             vTaskSuspend(xHandleLed);
         }
-        if (cont == 13){
+        if (cont == 8){
             vTaskResume(xHandleLed);
             cont = 0;
         }
